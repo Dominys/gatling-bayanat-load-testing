befdcbf5a56e5632f8b4886ad6ab39eab77869f1
rbezpalko <roman@fleetup.com>
2021-08-25 19:07:24 +0300

    Fix

--- src/test/scala/bayanat/BasicSimulation.scala
+++ src/test/scala/bayanat/BasicSimulation.scala
@@ -29,7 +29,7 @@ class BasicSimulation extends Simulation {
       .check(jsonPath("$..address").exists)
       .check(jsonPath("$..LongLabel").is("${address}")))
 
-  setUp(scn.inject(constantUsersPerSec(500).during(10.seconds))).throttle(
+  setUp(scn.inject(atOnceUsers(2000)).throttle(
     reachRps(100).in(5.seconds),
     holdFor(10.seconds),
     jumpToRps(200),
