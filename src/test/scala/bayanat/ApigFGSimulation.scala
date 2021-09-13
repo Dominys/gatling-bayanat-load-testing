b510d6a802132ace1f7913840b236938ab498959
rbezpalko <roman@fleetup.com>
2021-09-13 17:21:14 +0300

    Direct simulation update

--- src/test/scala/bayanat/ApigFGSimulation.scala
+++ src/test/scala/bayanat/ApigFGSimulation.scala
@@ -27,27 +27,29 @@ class ApigFGSimulation extends Simulation {
 
   setUp(scn.inject(atOnceUsers(10000),
 //                    nothingFor(15.minutes),
-                    constantUsersPerSec(300) during(5.minutes),
-                    constantUsersPerSec(400) during(5.minutes),
-                    constantUsersPerSec(500) during(5.minutes),
-                    constantUsersPerSec(600) during(5.minutes),
-                    constantUsersPerSec(700) during(5.minutes),
-                    constantUsersPerSec(800) during(5.minutes),
+                    rampUsersPerSec(1).to(300).during(5.minutes),
+                    rampUsersPerSec(300).to(1200).during(5.minutes),
+                    constantUsersPerSec(1200) during(10.minutes)
+//                    constantUsersPerSec(400) during(5.minutes),
+//                    constantUsersPerSec(500) during(5.minutes),
+//                    constantUsersPerSec(600) during(5.minutes),
+//                    constantUsersPerSec(700) during(5.minutes),
+//                    constantUsersPerSec(800) during(5.minutes),
 //                    nothingFor(30.minutes),
 //                    constantUsersPerSec(400) during(4.hours))
   )).throttle(
-    reachRps(300).in(5.seconds),
-    holdFor(5.minutes),
-    reachRps(400).in(5.seconds),
-    holdFor(5.minutes),
-    reachRps(500).in(5.seconds),
-    holdFor(5.minutes),
-    reachRps(600).in(5.seconds),
-    holdFor(5.minutes),
-    reachRps(700).in(5.seconds),
-    holdFor(5.minutes),
-    reachRps(800).in(5.seconds),
-    holdFor(5.minutes)
+    reachRps(300).in(5.minutes),
+    holdFor(5.seconds),
+//    reachRps(400).in(5.seconds),
+//    holdFor(5.minutes),
+//    reachRps(500).in(5.seconds),
+//    holdFor(5.minutes),
+//    reachRps(600).in(5.seconds),
+//    holdFor(5.minutes),
+//    reachRps(700).in(5.seconds),
+//    holdFor(5.minutes),
+    reachRps(1200).in(5.minutes),
+    holdFor(10.minutes)
 
     //    ,
 //    reachRps(0).in(30.minutes)
